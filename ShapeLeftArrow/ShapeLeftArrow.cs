cc70ef20ffedb267504840e3be9dc2520e839869
bonheurisme <100709185+unhi-dev-7012@users.noreply.github.com>
2024-05-03 15:30:06 +0700

    feat: define left/right arrow

---
+++ ShapeLeftArrow/ShapeLeftArrow.cs
@@ -0,0 +1,92 @@
+using BaseShapes;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+ 
+namespace ShapeLeftArrow
+{
+    public class ShapeLeftArrow: BaseShape
+    {
+        private Polygon leftArrow;
+        public ShapeLeftArrow()
+        {
+            _name = "LeftArrow";
+            _iconName = "left_arrow.png";
+        }
+        public override object Clone()
+        {
+            return MemberwiseClone();
+        }
+
+        public override Canvas Render()
+        {
+            _canvas = new Canvas();
+
+            double width = Math.Abs(_end.X - _start.X);
+            double height = Math.Abs(_end.Y - _start.Y);
+
+            leftArrow = new Polygon();
+            leftArrow.Fill = _colorFill;
+            leftArrow.Stroke = _colorStroke;
+            leftArrow.StrokeThickness = strokeThickness;
+            leftArrow.Points = CalculateRightArrowPoints(width, height);
+
+            _canvas.Children.Add(leftArrow);
+
+            return _canvas;
+        }
+
+        private PointCollection CalculateRightArrowPoints(double width, double height)
+        {
+            var points = new PointCollection()
+            {
+                 new System.Windows.Point(0 , height / 2),              // Point 1
+                new System.Windows.Point(width / 2, 0),                 // Point 2
+                new System.Windows.Point(width / 2, height / 4),        // Point 3
+                new System.Windows.Point(width, height / 4),            // Point 4
+                new System.Windows.Point(width, height * 0.75),         // Point 5
+                new System.Windows.Point(width / 2, height * 0.75),     // Point 6
+                new System.Windows.Point(width / 2, height),            // Point 7
+            };
+
+            return points;
+        }
+
+        public override void Resize()
+        {
+            if (leftArrow != null)
+            {
+                double width = Math.Abs(_end.X - _start.X);
+                double height = Math.Abs(_end.Y - _start.Y);
+
+                leftArrow.Points.Clear();
+                leftArrow.Points = CalculateRightArrowPoints(width, height);
+            }
+        }
+
+        public override void SetDashStroke(DoubleCollection dash)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SetStrokeColor(SolidColorBrush color)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SetStrokeFill(SolidColorBrush fill)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SetStrokeThickness(double thickness)
+        {
+            this.strokeThickness = thickness;
+            if (leftArrow != null)
+            {
+                leftArrow.StrokeThickness = thickness;
+            }
+        }
+    }
+
+}
---
+++ ShapeRightArrow/ShapeRightArrow.cs
@@ -0,0 +1,93 @@
+using BaseShapes;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+
+namespace ShapeRightArrow
+{
+    public class ShapeRightArrow : BaseShape
+    {
+        private Polygon rightArrow;
+        public ShapeRightArrow()
+        {
+            _name = "RightArrow";
+            _iconName = "right_arrow.png";
+        }
+        public override object Clone()
+        {
+            return MemberwiseClone();
+        }
+
+        public override Canvas Render()
+        {
+            _canvas = new Canvas();
+
+            double width = Math.Abs(_end.X - _start.X);
+            double height = Math.Abs(_end.Y - _start.Y);
+
+            rightArrow = new Polygon();
+            rightArrow.Fill = _colorFill;
+            rightArrow.Stroke = _colorStroke;
+            rightArrow.StrokeThickness = strokeThickness;
+            rightArrow.Points = CalculateRightArrowPoints(width, height);
+
+            _canvas.Children.Add(rightArrow);
+
+            return _canvas;
+        }
+
+        private PointCollection CalculateRightArrowPoints(double width, double height)
+        {
+            var points = new PointCollection()
+            {
+                new System.Windows.Point(width , height / 2),           // Point 1
+                new System.Windows.Point(width / 2, 0),                 // Point 2
+                new System.Windows.Point(width / 2, height / 4),        // Point 3
+                new System.Windows.Point(0, height / 4),                // Point 4
+                new System.Windows.Point(0, height * 0.75),             // Point 5
+                new System.Windows.Point(width / 2, height * 0.75),     // Point 6
+                new System.Windows.Point(width / 2, height),            // Point 7
+            };
+
+            return points;
+        }
+
+        public override void Resize()
+        {
+            if (rightArrow != null)
+            {
+                double width = Math.Abs(_end.X - _start.X);
+                double height = Math.Abs(_end.Y - _start.Y);
+
+                rightArrow.Points.Clear();
+                rightArrow.Points = CalculateRightArrowPoints(width, height); 
+            }
+        }
+
+        public override void SetDashStroke(DoubleCollection dash)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SetStrokeColor(SolidColorBrush color)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SetStrokeFill(SolidColorBrush fill)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SetStrokeThickness(double thickness)
+        {
+            this.strokeThickness = thickness;
+            if (rightArrow  != null)
+            {
+                rightArrow.StrokeThickness = thickness;
+            }
+        }
+    }
+
+}
